3faed2d6cc5bcf8717afa1c0353d7efe60c3d53e
esk <emseely@gmail.com>
2020-08-15 12:35:00 -0400

    FIX: router change to browserrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "./history";
+
 import App from "./App";
 
 // import { Provider } from "react-redux";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 // import store from "./store";
 
 ReactDOM.render(
   // <Provider store={store}>
-  <Router history={history}>
+  <Router>
     <App />
   </Router>,
   // </Provider>,
